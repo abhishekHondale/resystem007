6eb9faa8dfa4060cfa21c755521b7da1a73eae61
abhishekHondale <141109697+abhishekHondale@users.noreply.github.com>
2023-08-03 17:00:14 +0530

    Update resystem_app1.py

--- resys/resystem_app1.py
+++ resys/resystem_app1.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1U1f1BqGy014mUdaGJTvVl2Z3bZf8mlFW
 """
 
-import pickle
+from pickle5 import pickle 
 import pandas as pd
 import streamlit as st
 from streamlit import session_state as session
